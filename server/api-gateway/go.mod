d9cee01df7f11b122db693584e1bf9db9ac51762
code ngu nhất quả đất <72681678+code-ga@users.noreply.github.com>
2023-08-01 19:27:34 +0700

    add cdn ws proxy when get image ( impl view count for comic service )

--- server/api-gateway/go.mod
+++ server/api-gateway/go.mod
@@ -3,15 +3,16 @@ module api_gateway
 go 1.19
 
 require (
+	github.com/gofiber/contrib/websocket v1.1.0
 	github.com/gofiber/fiber/v2 v2.48.0
+	github.com/google/uuid v1.3.0
+	github.com/gorilla/websocket v1.5.0
 	github.com/valyala/fasthttp v1.48.0
 )
 
 require (
 	github.com/andybalholm/brotli v1.0.5 // indirect
 	github.com/fasthttp/websocket v1.5.4 // indirect
-	github.com/gofiber/contrib/websocket v1.1.0 // indirect
-	github.com/google/uuid v1.3.0 // indirect
 	github.com/klauspost/compress v1.16.7 // indirect
 	github.com/mattn/go-colorable v0.1.13 // indirect
 	github.com/mattn/go-isatty v0.0.19 // indirect
--- server/api-gateway/main.go
+++ server/api-gateway/main.go
@@ -9,16 +9,20 @@ import (
 	"path"
 	"strings"
 
+	fiberWebsocket "github.com/gofiber/contrib/websocket"
 	"github.com/gofiber/fiber/v2"
 	"github.com/gofiber/fiber/v2/middleware/compress"
 	"github.com/gofiber/fiber/v2/middleware/proxy"
+	"github.com/google/uuid"
 	"github.com/valyala/fasthttp"
 
 	"github.com/gofiber/fiber/v2/middleware/cors"
 	"github.com/gofiber/fiber/v2/middleware/logger"
+
+	"github.com/gorilla/websocket"
 )
 
-func setupProxy(app *fiber.App) {
+func setupProxy(app *fiber.App, serviceList []ServiceInfo) {
 	// if target https site uses a self-signed certificate, you should
 	// call WithTlsConfig before Do and Forward
 	proxy.WithTlsConfig(&tls.Config{
@@ -32,16 +36,6 @@ func setupProxy(app *fiber.App) {
 
 	api := app.Group("/api") // /api
 
-	configFile, err := os.ReadFile(path.Join(get__dirname(), "config.json"))
-	if err != nil {
-		panic(err)
-	}
-
-	var serviceList []ServiceInfo
-	err = json.Unmarshal([]byte(configFile), &serviceList)
-	if err != nil {
-		panic(err)
-	}
 	env_prefix := "env_"
 	// proxy.Forward
 	for _, v := range serviceList {
@@ -68,9 +62,30 @@ type ServiceInfo struct {
 	Endpoint string `json:"endpoint"`
 	Url      string `json:"url"`
 }
+type WsServiceInfo struct {
+	Endpoint string   `json:"endpoint"`
+	SendTo   string   `json:"sendTo"`
+	Headers  []string `json:"headers"`
+}
+
+type ConfigFileData struct {
+	Http []ServiceInfo   `json:"http"`
+	Ws   []WsServiceInfo `json:"ws"`
+}
 
 func main() {
 	app := fiber.New()
+
+	u := url.URL{
+		Scheme: "ws",
+		Host:   os.Getenv("WS_HOST") + ":" + os.Getenv("WS_PORT"),
+		Path:   "/",
+	}
+	c, _, err := websocket.DefaultDialer.Dial(u.String(), nil)
+	if err != nil {
+		log.Fatal("dial:", err)
+	}
+	defer c.Close()
 
 	app.Use(cors.New(cors.Config{
 		AllowHeaders:     "Origin,Content-Type,Accept,Content-Length,Accept-Language,Accept-Encoding,Connection,Access-Control-Allow-Origin",
@@ -81,8 +96,122 @@ func main() {
 	app.Use(logger.New())
 	app.Use(compress.New())
 
-	setupProxy(app)
+	configFileContent, err := os.ReadFile(path.Join(get__dirname(), "config.json"))
+	if err != nil {
+		panic(err)
+	}
+
+	var configFile ConfigFileData
+	err = json.Unmarshal([]byte(configFileContent), &configFile)
+	if err != nil {
+		panic(err)
+	}
+	setupProxy(app, configFile.Http)
+	setupWsProxy(app, c, configFile.Ws)
 	app.Listen(":" + os.Getenv("PORT"))
+}
+func setupWsProxy(app *fiber.App, ws *websocket.Conn, wsServiceList []WsServiceInfo) {
+	app.Use("/ws", func(c *fiber.Ctx) error {
+		// IsWebSocketUpgrade returns true if the client
+		// requested upgrade to the WebSocket protocol.
+		if fiberWebsocket.IsWebSocketUpgrade(c) {
+			c.Locals("allowed", true)
+			c.Locals("requestID", uuid.New().String())
+			return c.Next()
+		}
+		return fiber.ErrUpgradeRequired
+	})
+
+	for _, v := range wsServiceList {
+		app.Get("/ws"+v.Endpoint, fiberWebsocket.New(func(c *fiberWebsocket.Conn) {
+
+			requestID := c.Locals("requestID")
+			// websocket.Conn bindings https://pkg.go.dev/github.com/fasthttp/websocket?tab=doc#pkg-index
+			headers := map[string]string{}
+			for _, hn := range v.Headers {
+				headers[hn] = c.Headers(hn)
+			}
+			var (
+				mt  int
+				msg []byte
+				err error
+			)
+			for {
+				if mt, msg, err = c.ReadMessage(); err != nil {
+					log.Println("read:", err)
+					break
+				}
+				// send message and wait response
+				log.Printf("recv: %s", msg)
+				log.Printf("requestID: %v\n", requestID)
+				payload := WsRequestMessagePayload{}
+				wsRequestID := uuid.NewString()
+				wsRequest := WsRequest[WsRequestMessagePayload]{
+					Payload: payload,
+					Url:     v.SendTo,
+					From:    "api-gateway/wsGateway",
+					Header:  nil,
+					Type:    "message",
+					ID:      wsRequestID,
+				}
+				requestDataBytes, err := json.Marshal(wsRequest)
+				if err != nil {
+					log.Println("write:", err)
+					break
+				}
+				ws.WriteMessage(websocket.TextMessage, requestDataBytes)
+
+				for {
+					_, message, err := ws.ReadMessage()
+					if err != nil {
+						d, err := json.Marshal(map[string]interface{}{
+							"Error": "Server Decode Message Error",
+						})
+						if err != nil {
+							break
+						}
+						c.WriteMessage(mt, d)
+						break
+					}
+					var data WsReturnData[WsMessageResponse]
+					err = json.Unmarshal(message, &data)
+					if err != nil {
+						log.Println("write:", err)
+						d, err := json.Marshal(map[string]interface{}{
+							"Error": "Server Error",
+						})
+						if err != nil {
+							break
+						}
+						c.WriteMessage(mt, d)
+						break
+					}
+					if data.Type == "rep" {
+						if data.ID == wsRequestID {
+							if data.Error != nil {
+								d, err := json.Marshal(map[string]interface{}{
+									"Error": data.Error,
+								})
+								if err != nil {
+									break
+								}
+								c.WriteMessage(mt, d)
+								break
+							}
+							if err = c.WriteMessage(mt, data.Payload.Message); err != nil {
+								log.Println("write:", err)
+								break
+							}
+
+						}
+					}
+				}
+
+			}
+
+		}))
+	}
+
 }
 
 func ProxyHandler(path string) func(*fiber.Ctx) error {
@@ -111,4 +240,32 @@ func get__dirname() string {
 	}
 	exPath := path.Dir(ex)
 	return exPath
+}
+
+type WsRequest[pt any] struct {
+	Url     string       `json:"url"`
+	Header  *interface{} `json:"header"`
+	Payload pt           `json:"payload"`
+	From    string       `json:"from"`
+	Type    string       `json:"type"`
+	ID      string       `json:"id"`
+}
+type WsReturnData[T any] struct {
+	Url     string       `json:"url"`
+	Header  *interface{} `json:"header"`
+	Payload T            `json:"payload"`
+	Type    string       `json:"type"`
+	Error   *string      `json:"error"`
+	From    string       `json:"from"`
+	ID      string       `json:"id"`
+}
+
+type WsRequestMessagePayload struct {
+	Headers   map[string]string `json:"headers"`
+	RequestID string            `json:"requestID"`
+	Message   []byte            `json:"message"`
+}
+
+type WsMessageResponse struct {
+	Message []byte `json:"message"`
 }
