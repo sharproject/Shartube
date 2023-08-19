c43e348a6749b234a4f2e0aee0d0e8c309e0b83c
code ngu nhất quả đất <72681678+code-ga@users.noreply.github.com>
2023-08-19 14:18:31 +0700

    update get env

--- server/like-service/src/util/get_db_url.rs
+++ server/like-service/src/util/get_db_url.rs
@@ -1,22 +1,25 @@
 use mongodb::options::{ClientOptions, ServerApi, ServerApiVersion};
+use std::env;
 
 pub async fn get_db_url() -> ClientOptions {
-    let db_name = option_env!("DB_NAME").unwrap_or("likes");
-    let db_username = option_env!("DB_USERNAME").unwrap_or("root");
-    let db_password = option_env!("DB_PASSWORD").unwrap_or("root");
-    let db_host = option_env!("DB_HOST").unwrap_or("localhost");
-    let db_port = option_env!("DB_PORT");
+    let db_name = env::var("DB_NAME").unwrap_or("likes".to_string());
+    let db_username = env::var("DB_USERNAME").unwrap_or("root".to_string());
+    let db_password = env::var("DB_PASSWORD").unwrap_or("root".to_string());
+    let db_host = env::var("DB_HOST").unwrap_or("localhost".to_string());
+    let db_port = env::var("DB_PORT");
     let client_options = match db_port {
-        Some(port) => ClientOptions::parse_async(format!(
+        Ok(port) => ClientOptions::parse_async(format!(
             "mongodb://{}:{}@{}:{}/?authSource=admin&readPreference=primary&ssl=false",
             db_username, db_password, db_host, port
-        )).await
+        ))
+        .await
         .unwrap(),
-        None => {
+        Err(_) => {
             let mut client_options = ClientOptions::parse_async(format!(
                 "mongodb+srv://{}:{}@{}/{}?retryWrites=true&w=majority",
                 db_username, db_password, db_host, db_name
-            )).await
+            ))
+            .await
             .unwrap();
             // Set the server_api field of the client_options object to Stable API version 1
             let server_api = ServerApi::builder().version(ServerApiVersion::V1).build();
