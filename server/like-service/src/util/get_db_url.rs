3b3cf5d266f40e90d706e8a7f4f289294763907d
code ngu nhất quả đất <72681678+code-ga@users.noreply.github.com>
2023-08-19 12:40:22 +0700

    try some solution

--- server/like-service/src/util/get_db_url.rs
+++ server/like-service/src/util/get_db_url.rs
@@ -7,16 +7,16 @@ pub async fn get_db_url() -> ClientOptions {
     let db_host = option_env!("DB_HOST").unwrap_or("localhost");
     let db_port = option_env!("DB_PORT");
     let client_options = match db_port {
-        Some(port) => ClientOptions::parse(format!(
+        Some(port) => ClientOptions::parse_async(format!(
             "mongodb://{}:{}@{}:{}/?authSource=admin&readPreference=primary&ssl=false",
             db_username, db_password, db_host, port
-        ))
+        )).await
         .unwrap(),
         None => {
-            let mut client_options = ClientOptions::parse(format!(
+            let mut client_options = ClientOptions::parse_async(format!(
                 "mongodb+srv://{}:{}@{}/{}?retryWrites=true&w=majority",
                 db_username, db_password, db_host, db_name
-            ))
+            )).await
             .unwrap();
             // Set the server_api field of the client_options object to Stable API version 1
             let server_api = ServerApi::builder().version(ServerApiVersion::V1).build();
