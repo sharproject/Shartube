7e98a653bb4e8fe74ee270f6c7b19b6279e37ef7
code-ga <tritranduc11@gmail.com>
2023-12-13 22:47:55 +0700

    refactor like service #41 ( user )
    - remove some things
    - untested commit

--- server/comic/directives/auth.go
+++ server/comic/directives/auth.go
@@ -31,51 +31,6 @@ func AuthDirective(ctx context.Context, _ interface{}, next graphql.Resolver) (i
 
 	teamID := request.Header.Clone().Get("team-id")
 	fmt.Printf("teamID: %v\n", teamID)
-	// requestId := uuid.New().String()
-	// payload := struct {
-	// 	Token  string `json:"token"`
-	// 	ID     string `json:"id"`
-	// 	TeamId string `json:"teamID"`
-	// }{
-	// 	Token:  auth,
-	// 	ID:     requestId,
-	// 	TeamId: teamID,
-	// }
-	// requestData := LocalTypes.ServiceRequest{
-	// 	Url:     "user/decodeToken",
-	// 	Header:  nil,
-	// 	Payload: &payload,
-	// 	From:    "comic/auth",
-	// 	Type:    "message",
-	// 	ID:      requestId,
-	// }
-
-	// data, err := util.ServiceSender[LocalTypes.AuthPayloadReturn, *interface{}](redis, requestData, true)
-	// if err != nil {
-	// 	return nil, err
-	// }
-
-	// if data.Type == "rep" {
-	// 	if data.ID == requestId && data.Url == requestData.From && data.From == requestData.Url {
-	// 		if data.Error != nil {
-	// 			return nil, &gqlerror.Error{
-	// 				Message: "Access Denied",
-	// 			}
-	// 		}
-	// 		if data.Payload.SessionData != nil {
-	// 			return next(context.WithValue(ctx, AuthString("session"), data.Payload.SessionData))
-	// 		}
-
-	// 		return nil, &gqlerror.Error{
-	// 			Message: "Access Denied",
-	// 		}
-
-	// 	}
-	// }
-
-	// return nil, &gqlerror.Error{
-	// 	Message: "500 Server Error",
-	// }
 	// make request to user server to decode token using the header authorization and team-id
 	req, err := http.NewRequest(http.MethodGet, "http://shartube-user-server:8080/private/decodeToken", bytes.NewBuffer([]byte(nil)))
 	if err != nil {
--- server/like-service/src/util/auth.rs
+++ server/like-service/src/util/auth.rs
@@ -1,71 +1,63 @@
-use crate::types::{AuthPayloadReturn, AuthSessionDataReturn, RedisClient, SenderData};
-
-use super::service::send_service_message;
+use crate::types::{AuthPayloadReturn, AuthSessionDataReturn, RedisClient};
+use reqwest::header;
 
 pub async fn get_user_session(
     token: &str,
-    redis_client: actix_web::web::Data<RedisClient>,
+    _redis_client: actix_web::web::Data<RedisClient>,
 ) -> Option<AuthSessionDataReturn> {
-    let message_id = uuid::Uuid::new_v4().to_string();
-    let sender_data = SenderData {
-        url: "user/decodeToken".to_string(),
-        message_type: "message".to_string(),
-        from: "like/auth".to_string(),
-        header: serde_json::Value::Null,
-        payload: serde_json::json! {{
-            "token":token,
-            "id":message_id.clone()
-        }},
-        error: serde_json::Value::Null,
-        id: message_id.clone(),
-    };
-    // let mut socket = socket.lock().unwrap();
-    // socket
-    //     .write_message(tungstenite::Message::Text(
-    //         serde_json::to_string(&sender_data).unwrap(),
-    //     ))
-    //     .unwrap();
-    // loop {
-    //     let message = socket.read_message().unwrap();
-    //     if let tungstenite::Message::Text(text) = message {
-    //         let json_data = if let Ok(data) = serde_json::from_str::<SenderData>(&text) {
-    //             data
-    //         } else {
-    //             continue;
-    //         };
-    //         if json_data.message_type != "req" {
-    //             continue;
-    //         }
-    //         if let Some(id) = json_data.payload.get("id") {
-    //             if id.as_str().unwrap() != message_id {
-    //                 continue;
-    //             }
-    //         } else {
-    //             continue;
-    //         }
-
-    //         if json_data.from != "user/decodeToken" {
-    //             continue;
-    //         }
-    //         if !json_data.error.is_null() {
-    //             return None;
-    //         }
-    //         return serde_json::from_str::<SenderData<AuthPayloadReturn>>(&text)
-    //             .unwrap()
-    //             .payload
-    //             .session_data;
-    //     }
+    // let message_id = uuid::Uuid::new_v4().to_string();
+    // let sender_data = SenderData {
+    //     url: "user/decodeToken".to_string(),
+    //     message_type: "message".to_string(),
+    //     from: "like/auth".to_string(),
+    //     header: serde_json::Value::Null,
+    //     payload: serde_json::json! {{
+    //         "token":token,
+    //         "id":message_id.clone()
+    //     }},
+    //     error: serde_json::Value::Null,
+    //     id: message_id.clone(),
+    // };
+    // let response =
+    //     send_service_message::<AuthPayloadReturn>(&redis_client.clone(), &sender_data, true)
+    //         .await
+    //         .unwrap();
+    // if response.is_none() {
+    //     return None;
     // }
-    let response =
-        send_service_message::<AuthPayloadReturn>(&redis_client.clone(), &sender_data, true)
-            .await
-            .unwrap();
-    if response.is_none() {
+    // let response = response.unwrap();
+    // if !response.error.is_null() {
+    //     return None;
+    // }
+    // return response.payload.session_data;
+    let url = "http://shartube-user-server:8080/private/decodeToken";
+    let client = reqwest::Client::new();
+    let req_builder = client.get(url);
+    let req = match req_builder
+        .body(Vec::new())
+        .header(header::AUTHORIZATION, token)
+        .header(header::CONTENT_TYPE, "application/json")
+        .build()
+    {
+        Ok(req) => req,
+        Err(_) => return None,
+    };
+    let resp = match client.execute(req).await {
+        Ok(resp) => resp,
+        Err(_) => return None,
+    };
+
+    if !resp.status().is_success() {
         return None;
     }
-    let response = response.unwrap();
-    if !response.error.is_null() {
-        return None;
+
+    let payload: AuthPayloadReturn = match resp.json().await {
+        Ok(payload) => payload,
+        Err(_) => return None,
+    };
+    if let Some(session_data) = payload.session_data {
+        return Some(session_data);
     }
-    return response.payload.session_data;
+
+    None
 }
